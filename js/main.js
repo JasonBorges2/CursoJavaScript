867bbcb2c416ba6eebf72a0ded6ab837f0f0c765
Jason Borges <jgbs6@outlook.com>
2022-03-30 23:53:48 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+function botao(){
+    alert("Obrigado por clicar");
+}
+
+/*var validar;
+function validaidade(idade){
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+var idade = prompt("Qual é a sua idade? ");
+validaidade(idade);
+console.log(validar);
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, new_name){
+    return frase.replace(nome, new_name)
+}
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+var d = new Date();
+alert(d.getMonth()+1); // coloca +1 pois o month ele conta do zero
+alert(d.getDate()) // pega o dia
+alert(d.getDay()); //pega o dia da semana
+alert(d.getFullYear()); //pega o ano
+alert(d.getHours()); //pega a hora
+alert(d.getMinutes()); //pega minutos
+alert(d.getMilliseconds()); //pega milisegundos
+
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+}
+
+var idade = prompt("Qual a sua idade? ");
+if(idade > 18){
+    alert("Maior de idade");
+
+}else{
+    alert("Menor de idade");
+}
+
+var frutas = [{nome:"Maçã", cor:"vermelha"}, {nome:"Uva", cor:"roxa"}];
+console.log(frutas[0].nome);
+
+var fruta = {nome:"Maçã", cor:"vermelha"};
+console.log(fruta.nome);
+
+var lista = [" maçã" , " pêra" , " laranja"];
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop(lista[3]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" | "));
+
+var nome = "Jason Glayson"; 
+var idade = 22;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert("Bem vindo" + nome + idade);
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2)
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+*/
+
+
+
